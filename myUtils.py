1b1231ecc9ce06f9e18d000eafaf5173fae1e69b
ChadG <gibeautc@oregonstate.edu>
2018-08-27 16:59:13 -0700

    update

--- myUtils.py
+++ myUtils.py
@@ -10,25 +10,16 @@ def stripWord(word):
 	'''
 	returns the word after processing it, it stips white space, removes punctuation, and converts to lower case
 	'''
+	for x in range(10):
+		word=word.replace(str(x),"")
+
 	word=word.strip()
 	word=word.lower()
 	word = word.translate(table) 
 	return word
 
-
-
-
-
-
-
-
-
-
-
-
-
 if __name__=="__main__":
-	words=['Hello',"GoodBye","[yep]","s ome spaces   ",""," ","it's"]
+	words=['Hello',"GoodBye","[yep]","s ome spaces   ",""," ","it's","2018","Hell0"]
 	print(words)
 	new=[]
 	for w in words:
